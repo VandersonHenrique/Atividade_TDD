f8bfb784b9a8fe5f0f20387b2b2cdfb5248ac647
fernandorufinobarcelos <129413508+fernandorufinobarcelos@users.noreply.github.com>
2025-05-22 16:51:21 +0000

    Adicionado verificação de digito, caractere maiusculo, caractere especial e lida com multiplos erros, também foi atualizado os testes realizados

--- Validador_senha/Main.java
+++ Validador_senha/Main.java
@@ -1,16 +1,50 @@
 public class Main{
     public static void main(String[] args) {
-        String senha = "abc123";
-        String senha2 = "abc12345678";
 
+        String senha1 = "mypass";
+        String senha2 = "Mypass12!";
+        String senha3 = "MypassMypass!";
+        String senha4 = "mypassmypass12!";
+        String senha5 = "MypassMypass12";
+        String senha6 = "MypassMypass12!";
 
-        if (Teste_senha.validar(senha)) {
+        System.out.println("Teste de senha que não segue nenhum dos requisitos:");
+        if (Teste_senha.validar(senha1)) {
             System.out.println("Senha válida!");
         } else {
             System.out.println("Senha inválida.");
         }
         
+        System.out.println("Teste de senha que possui menos de 8 caracteres:");
         if (Teste_senha.validar(senha2)) {
+            System.out.println("Senha válida!");
+        } else {
+            System.out.println("Senha inválida.");
+        }
+        
+        System.out.println("Teste de senha que não possui 2 dígitos:");
+        if (Teste_senha.validar(senha3)) {
+            System.out.println("Senha válida!");
+        } else {
+            System.out.println("Senha inválida.");
+        }
+        
+        System.out.println("Teste de senha que não possui letra maiúscula:");
+        if (Teste_senha.validar(senha4)) {
+            System.out.println("Senha válida!");
+        } else {
+            System.out.println("Senha inválida.");
+        }
+        
+        System.out.println("Teste de senha que não possui caractere especial:");
+        if (Teste_senha.validar(senha5)) {
+            System.out.println("Senha válida!");
+        } else {
+            System.out.println("Senha inválida.");
+        }
+        
+        System.out.println("Teste de senha que segue todos os requisitos:");
+        if (Teste_senha.validar(senha6)) {
             System.out.println("Senha válida!");
         } else {
             System.out.println("Senha inválida.");
--- Validador_senha/Teste_senha.java
+++ Validador_senha/Teste_senha.java
@@ -1,11 +1,56 @@
 public class Teste_senha {
 
     
+    
     public static boolean validar(String senha) {
+        
+        boolean validacao = true;
+
+        // valida se a senha possui ao menos 8 caracteres
         if (senha.length() < 8) {
             System.out.println("A senha deve ter pelo menos 8 caracteres");
-            return false;
+            validacao = false;
         }
-        return true;
+        // valida se a senha possui ao menos 2 digitos
+        if (!possuiDigito(senha)){
+            System.out.println("A senha deve conter pelo menos 2 dígitos");
+            validacao = false;
+        }
+        // valida se a senha possui ao menos 1 letra maiuscula
+        if (!letraMaiuscula(senha)){
+            System.out.println("A senha deve conter pelo menos uma letra maiuscula");
+            validacao = false;
+        }
+        // valida se a senha possui ao menos 1 caractere especial
+        if (!senha.matches(".*[^a-zA-Z0-9].*")){
+            System.out.println("A senha deve conter pelo menos um caractere especial");
+            validacao = false;
+        }
+        return validacao;
     }
+
+    public static boolean possuiDigito(String senha){
+        int contador = 0;
+
+        for (char c : senha.toCharArray()) {
+            if (Character.isDigit(c)) {
+                contador++;
+                if (contador >= 2) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static boolean letraMaiuscula(String senha) {
+        for (char c : senha.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
